18e0fe15929186a129496fef1bf5b2bcd9b0b64a
LifeIT-web <jyi1102@hotmail.com>
2022-10-03 14:38:51 +0900

    basic1010 ~ basic1015

---
+++ selfstudy/src/basic1014/Main.java
@@ -0,0 +1,12 @@
+package src.basic1014;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char x = sc.next().charAt(0);
+        char y = sc.next().charAt(0);
+        System.out.println(y + " " + x);
+    }
+}
