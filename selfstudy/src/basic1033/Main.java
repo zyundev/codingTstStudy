655034530e06176a14788e8ccfde6e95c0744d13
LifeIT-web <jyi1102@hotmail.com>
2022-10-09 13:13:00 +0900

    basic1029 ~ basic1037

---
+++ selfstudy/src/basic1033/Main.java
@@ -0,0 +1,11 @@
+package src.basic1033;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.printf("%X", i);
+    }
+}
---
+++ selfstudy/src/basic1035/Main.java
@@ -0,0 +1,12 @@
+package src.basic1035;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int hex = Integer.parseInt(s, 16); // 16진수를 10진수로 변환
+        System.out.printf("%o", hex);
+    }
+}
