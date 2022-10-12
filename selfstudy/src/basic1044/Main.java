793066974847dcf2b30bfdd52adf398a22b5f7d1
LifeIT-web <jyi1102@hotmail.com>
2022-10-12 21:49:01 +0900

    basic1044 ~ basic1046

---
+++ selfstudy/src/basic1044/Main.java
@@ -0,0 +1,11 @@
+package src.basic1044;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextInt();
+        System.out.println(++x);
+    }
+}
