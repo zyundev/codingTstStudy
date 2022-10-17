0c7010ecdb847f0fcc39399ed961f4815df975f7
LifeIT-web <jyi1102@hotmail.com>
2022-10-17 20:08:46 +0900

    basic1049 ~ basic1050

---
+++ selfstudy/src/basic1049/Main.java
@@ -0,0 +1,13 @@
+package src.basic1049;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int flag = x > y ? 1 : 0;
+        System.out.println(flag);
+    }
+}
