f67a97a07a9da8d4fb634eea7e142200a39ed801
LifeIT-web <jyi1102@hotmail.com>
2022-10-19 20:19:05 +0900

    basic1078 ~ basic1079

---
+++ selfstudy/src/basic1079/Main.java
@@ -0,0 +1,16 @@
+package src.basic1079;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            char c = sc.next().charAt(0);
+            System.out.println(c);
+            if (c == 'q') {
+                break;
+            }
+        }
+    }
+}
