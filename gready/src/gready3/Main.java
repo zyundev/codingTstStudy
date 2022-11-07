5701c277cf4bfcf44035cfb0effbffd957b9f2b8
LifeIT-web <jyi1102@hotmail.com>
2022-11-07 20:07:37 +0900

    gready3

---
+++ gready/src/gready3/Main.java
@@ -0,0 +1,15 @@
+package src.gready3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if (x % 4 == 0 && (x % 100 != 0 || x % 400 == 0)){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
